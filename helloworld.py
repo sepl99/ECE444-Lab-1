f80209eae3ea315783cda53745b9b380aa03eba9
sepl99-2 <91038120+sepl99-2@users.noreply.github.com>
2021-09-19 21:49:16 -0400

    Partner made change to python script

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,1 @@
-print("Hell World. My Name is Sam Eplett. I have been at UofT for 4 years.")
+print("Hell World. My Name is Sam Eplett. I have been at UofT for 4 years.  THIS SCRIPT WAS CHANGED BY MY PARTNER")
